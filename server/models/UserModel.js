7550dadccc30ebcf1004da58ee42a8c446684058
IMRANDIL <aliimranadil2@gmail.com>
2022-07-11 18:00:29 +0530

    yh

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,24 +1,37 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    profilePicture: String,
+    coverPicture: String,
+    about: String,
+    livesin: String,
+    worksAt: String,
+    relationship: String,
+    followers: [],
+    following: [],
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  firstname: {
-    type: String,
-    required: true,
-  },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 
-router.route("/").post(authController);
+// router.route("/").post(authController);
 
 module.exports = router;
