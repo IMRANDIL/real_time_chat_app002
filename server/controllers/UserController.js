ab9545bed09993a58ec3c70504302005fc972d0c
IMRANDIL <aliimranadil2@gmail.com>
2022-07-12 13:57:41 +0530

    yh

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -18,3 +18,26 @@ exports.getUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+//update a user
+
+exports.updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { currentUserId, currentUserAdminStatus } = req.body;
+  try {
+    if (id === currentUserId || currentUserAdminStatus) {
+      const user = await User.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
