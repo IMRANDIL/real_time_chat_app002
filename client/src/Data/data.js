fd78299108dbac029911dd54c661125be398fe2a
IMRANDIL <aliimranadil2@gmail.com>
2022-07-14 14:18:36 +0530

    yh

---
+++ client/src/Data/data.js
@@ -0,0 +1,31 @@
+import img1 from "../img/img1(1).png";
+import img2 from "../img/img2(1).png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+  {
+    id: 1,
+    name: "Andrew Thomas",
+    username: "Andrewya",
+    img: img1,
+  },
+  {
+    id: 2,
+    name: "Thomas loka",
+    username: "Lokya",
+    img: img2,
+  },
+  {
+    id: 3,
+    name: "Toast Naka",
+    username: "Toastya",
+    img: img3,
+  },
+  {
+    id: 4,
+    name: "Luka Manas",
+    username: "Manasya",
+    img: img4,
+  },
+];
