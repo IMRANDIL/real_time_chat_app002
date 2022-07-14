f0cd145fe597199d8cf065c0f8a99d9f7e28d646
IMRANDIL <aliimranadil2@gmail.com>
2022-07-14 14:24:39 +0530

    yh

--- client/src/Data/data.js
+++ client/src/Data/data.js
@@ -1,5 +1,5 @@
-import img1 from "../img/img1(1).png";
-import img2 from "../img/img2(1).png";
+import img1 from "../img/img1 (1).png";
+import img2 from "../img/img2 (1).png";
 import img3 from "../img/img3.png";
 import img4 from "../img/img4.jpg";
 
--- client/src/components/FollowersCard/FollowersCard.jsx
+++ client/src/components/FollowersCard/FollowersCard.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./FollowersCard.css";
+import { Followers } from "../../Data/data";
 
 const FollowersCard = () => {
   return (
     <div className="followersCard">
       <h3>Who is following you</h3>
+
+      {Followers.map((follower) => (
+        <div className="follower" key={follower.id}>
+          <div>
+            <img src={follower.img} alt={follower.username} />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
