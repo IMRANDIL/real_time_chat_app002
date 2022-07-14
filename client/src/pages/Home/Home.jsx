3c45e0bd0ed58902374b74b23f53ca876982f21c
IMRANDIL <aliimranadil2@gmail.com>
2022-07-14 09:29:57 +0530

    yh

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="profileSide">Profile</div>
+      <div className="postSide">Post</div>
+      <div className="rightSide">Right</div>
+    </div>
+  );
 };
 
 export default Home;
