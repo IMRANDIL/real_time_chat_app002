61262443ee726d8a9d7b84839cbee71be1f59640
IMRANDIL <aliimranadil2@gmail.com>
2022-07-14 10:58:07 +0530

    yh

--- client/src/components/LogoSearch/LogoSearch.jsx
+++ client/src/components/LogoSearch/LogoSearch.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./LogoSearch.css";
 import Logo from "../../img/logo.png";
+import { UilSearch } from "react-icons/uil";
 
 const LogoSearch = () => {
   return (
     <div className="logoSearch">
       <img src={Logo} alt="logo-img" />
+      <div className="Search">
+        <input type="text" placeholder="#Explore" />
+        <div className="s-icon">
+          <UilSearch />
+        </div>
+      </div>
     </div>
   );
 };
