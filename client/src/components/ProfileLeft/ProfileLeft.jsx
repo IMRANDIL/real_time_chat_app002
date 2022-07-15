29827d5b337dedbac6ff3ac88ba5679bae51b2e3
IMRANDIL <aliimranadil2@gmail.com>
2022-07-15 17:08:40 +0530

    yh

--- client/src/components/ProfileLeft/ProfileLeft.jsx
+++ client/src/components/ProfileLeft/ProfileLeft.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./ProfileLeft.css";
+import LogoSearch from "../../components/LogoSearch/LogoSearch";
+import FollowersCard from "../../components/FollowersCard/FollowersCard";
+import InfoCard from "../InfoCard/InfoCard";
 
 const ProfileLeft = () => {
-  return <div>ProfileLeft</div>;
+  return (
+    <div className="profileLeft">
+      <LogoSearch />
+      <InfoCard />
+      <FollowersCard />
+    </div>
+  );
 };
 
 export default ProfileLeft;
