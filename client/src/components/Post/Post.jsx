2bfabebd1ffbed48ed7efd5503af83c65234acef
IMRANDIL <aliimranadil2@gmail.com>
2022-07-15 08:07:09 +0530

    yh

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Post.css";
+import Comment from "../../img/comment.png";
+import Share from "../../img/share.png";
+import Heart from "../../img/like.png";
+import NotLike from "../../img/notlike.png";
 
 const Post = ({ post }) => {
   return (
     <div className="post">
       <img src={post.img} alt="post-img" />
+      <div className="postReact">
+        <img src={post.liked ? Heart : NotLike} alt="like-unlike-img" />
+        <img src={Comment} alt="comment-img" />
+        <img src={Share} alt="share-img" />
+      </div>
+      <span>{post.likes} likes</span>
+      <div className="detail">
+        <span>
+          <b>{post.name}</b>
+        </span>
+        <span> {post.des}</span>
+      </div>
     </div>
   );
 };
