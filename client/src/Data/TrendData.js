f93de9457b385cb7e2292a5d8a0a97f6ba8f4b35
IMRANDIL <aliimranadil2@gmail.com>
2022-07-15 10:34:14 +0530

    yh

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,27 @@
+export const TrendData = [
+  {
+    id: 1,
+    name: "Limion",
+    shares: 98,
+  },
+  {
+    id: 2,
+    name: "Tomios",
+    shares: 90,
+  },
+  {
+    id: 3,
+    name: "Lavanya",
+    shares: 87,
+  },
+  {
+    id: 4,
+    name: "Srinivas",
+    shares: 72,
+  },
+  {
+    id: 5,
+    name: "Elioaa",
+    shares: 57,
+  },
+];
--- client/src/components/TrendCard/TrendCard.jsx
+++ client/src/components/TrendCard/TrendCard.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./TrendCard.css";
+import { TrendData } from "../../Data/TrendData";
 
 const TrendCard = () => {
-  return <div className="trendCard">TrendCard</div>;
+  return (
+    <div className="trendCard">
+      <h3>Trends for you</h3>
+
+      {TrendData.map((trend) => (
+        <div className="trend" key={trend.id}>
+          <span>#{trend.name}</span>
+          <span>{trend.shares}k shares</span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TrendCard;
