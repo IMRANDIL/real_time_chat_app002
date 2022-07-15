f900df6b78d861235d515280b9ffb9e1f0c72419
IMRANDIL <aliimranadil2@gmail.com>
2022-07-15 16:59:17 +0530

    yh

---
+++ client/src/pages/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfileLeft from "../../components/ProfileLeft/ProfileLeft";
+import "./Profile.css";
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <ProfileLeft />
+    </div>
+  );
+};
+
+export default Profile;
