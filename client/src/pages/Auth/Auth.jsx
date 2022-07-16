ffc53ca2bc766ddc486394f5945e99f92f2d5279
IMRANDIL <aliimranadil2@gmail.com>
2022-07-16 10:33:51 +0530

    yh

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import LogoImg from "../../img/logo.png";
-import { Link } from "react-router-dom";
 import "./Auth.css";
+import Signup from "./Signup";
 const Auth = () => {
   return (
     <div className="auth">
@@ -16,65 +16,5 @@ const Auth = () => {
     </div>
   );
 };
-
-function Signup() {
-  return (
-    <div className="auth-right">
-      <form className="info auth-form">
-        <h3>Sign up</h3>
-        <div>
-          <input
-            type="text"
-            placeholder="First Name"
-            className="infoInput"
-            name="firstname"
-            required
-          />
-
-          <input
-            type="text"
-            placeholder="Last Name"
-            className="infoInput"
-            name="lastname"
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            className="infoInput"
-            name="username"
-            placeholder="Username"
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            className="infoInput"
-            name="password"
-            placeholder="Password"
-            required
-          />
-          <input
-            type="password"
-            className="infoInput"
-            name="password"
-            placeholder="Confirm Password"
-            required
-          />
-        </div>
-        <button type="submit" className="button auth-button">
-          Signup
-        </button>
-        <div className="confirmbee">
-          <span>
-            Already a bee? <Link to="/login">Login</Link>
-          </span>
-        </div>
-      </form>
-    </div>
-  );
-}
 
 export default Auth;
---
+++ client/src/pages/Auth/Signup.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Auth.css";
+import { Link } from "react-router-dom";
+
+function Signup() {
+  return (
+    <div className="auth-right">
+      <form className="info auth-form">
+        <h3>Sign up</h3>
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            className="infoInput"
+            name="firstname"
+            required
+          />
+
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="infoInput"
+            name="lastname"
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            name="username"
+            placeholder="Username"
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            className="infoInput"
+            name="password"
+            placeholder="Password"
+            required
+          />
+          <input
+            type="password"
+            className="infoInput"
+            name="password"
+            placeholder="Confirm Password"
+            required
+          />
+        </div>
+        <button type="submit" className="button auth-button">
+          Signup
+        </button>
+        <div className="confirmbee">
+          <span>
+            Already a bee? <Link to="/login">Login</Link>
+          </span>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
