683dd36cd7d450caf120b4b30ae2b064b6770559
IMRANDIL <aliimranadil2@gmail.com>
2022-07-17 10:42:52 +0530

    yh

--- server/Middlewares/AuthMiddleware.js
+++ server/Middlewares/AuthMiddleware.js
@@ -1,1 +1,27 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.protectRoute = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Authorization Failed,Invalid Token");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Authorization Failed, No Token");
+  }
+};
