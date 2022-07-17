01cd78939ac65fa804a8e7dd2f534b84f00ff8f7
IMRANDIL <aliimranadil2@gmail.com>
2022-07-17 11:26:33 +0530

    yh

--- client/src/components/PostShare/PostShare.jsx
+++ client/src/components/PostShare/PostShare.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import "./PostShare.css";
 import ProfileImg from "../../img/profileImg.jpg";
+import { useSelector } from "react-redux";
 import { UilScenery } from "@iconscout/react-unicons";
 import { UilPlayCircle } from "@iconscout/react-unicons";
 import { UilLocationPoint } from "@iconscout/react-unicons";
@@ -9,9 +10,9 @@ import { UilTimes } from "@iconscout/react-unicons";
 
 const PostShare = () => {
   const [image, setImage] = useState(null);
-
+  const { userInfo } = useSelector((state) => state.registerUser);
   const imageRef = useRef();
-
+  const desc = useRef();
   const handleImage = (e) => {
     if (e.target.files && e.target.files[0]) {
       let img = e.target.files[0];
@@ -19,11 +20,33 @@ const PostShare = () => {
     }
   };
 
+  const handleShare = (e) => {
+    e.preventDefault();
+    const newPost = {
+      userId: userInfo._id,
+      description: desc.current.value,
+    };
+
+    if (image) {
+      const data = new FormData();
+      const filename = Date.now() + "-" + image.name;
+      data.append("name", filename);
+      data.append("file", image);
+      newPost.image = filename;
+      console.log(newPost);
+    }
+  };
+
   return (
     <div className="postShare">
       <img src={ProfileImg} alt="profile-img" draggable="false" />
       <div>
-        <input type="text" placeholder="What's in your mind?" />
+        <input
+          type="text"
+          placeholder="What's in your mind?"
+          ref={desc}
+          required
+        />
         <div className="postOptions">
           <div
             className="option"
@@ -45,7 +68,9 @@ const PostShare = () => {
             <UilSchedule />
             Shedule
           </div>
-          <button className="button ps-button">Share</button>
+          <button className="button ps-button" onClick={handleShare}>
+            Share
+          </button>
           <div style={{ display: "none" }}>
             <input
               type="file"
