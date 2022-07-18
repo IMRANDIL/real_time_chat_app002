5bb12f365775f68ad6ba522db7380bbad1dacc35
IMRANDIL <aliimranadil2@gmail.com>
2022-07-18 10:46:51 +0530

    yh

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Post.css";
 import Comment from "../../img/comment.png";
+import { useSelector } from "react-redux";
 import Share from "../../img/share.png";
 import Heart from "../../img/like.png";
 import NotLike from "../../img/notlike.png";
+import NoImg from "../../img/noImg.png";
 
 const Post = ({ post }) => {
+  const { userInfo } = useSelector((state) => state.registerUser);
+  const [like, setLike] = useState(post.likes.includes(userInfo._id));
+  const [numLike, setNumLike] = useState(post.likes.length);
+
+  const handleLike = () => {
+    setLike((prev) => !prev);
+    numLike ? setNumLike((prev) => prev - 1) : setNumLike((prev) => prev + 1);
+  };
+
   return (
     <div className="post">
       <img
-        src={post.image && process.env.REACT_APP_PUBLIC_FOLDER + post.image}
+        src={
+          post.image ? process.env.REACT_APP_PUBLIC_FOLDER + post.image : NoImg
+        }
         alt="post-img"
         draggable="false"
       />
       <div className="postReact">
-        <img src={post.liked ? Heart : NotLike} alt="like-unlike-img" />
+        <img
+          src={like ? Heart : NotLike}
+          alt="like-unlike-img"
+          onClick={handleLike}
+        />
         <img src={Comment} alt="comment-img" draggable="false" />
         <img src={Share} alt="share-img" draggable="false" />
       </div>
       <span style={{ color: "var(--gray)", fontSize: "12px" }}>
-        {post.likes} likes
+        {numLike} likes
       </span>
       <div className="detail">
         <span>
