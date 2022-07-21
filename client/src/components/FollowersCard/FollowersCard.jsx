babc75b6188ddd0e3726c7feaadb2ac9469054fb
IMRANDIL <aliimranadil2@gmail.com>
2022-07-21 08:17:13 +0530

    yh

--- client/src/components/FollowersCard/FollowersCard.jsx
+++ client/src/components/FollowersCard/FollowersCard.jsx
@@ -8,12 +8,15 @@ import User from "../User/User";
 
 const FollowersCard = () => {
   const [Followers, setFollowers] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { userInfo } = useSelector((state) => state.registerUser);
   useEffect(() => {
     const fetchUsers = async () => {
       try {
+        setLoading(true);
         const { data } = await axios.get(`http://localhost:5000/user`);
         setFollowers(data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
         toast.error(error);
@@ -25,7 +28,7 @@ const FollowersCard = () => {
   return (
     <div className="followersCard">
       <h3>People you may know</h3>
-
+      {loading && <h3>Loading...</h3>}
       {Followers?.map((follower) => {
         if (follower._id !== userInfo._id) {
           return <User follower={follower} key={follower._id} />;
