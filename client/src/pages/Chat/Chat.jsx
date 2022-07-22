78152c645ef8a9c21a0b959600276c7ed6bfffd4
IMRANDIL <aliimranadil2@gmail.com>
2022-07-22 09:26:32 +0530

    yh

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Chat.css";
+import LogoSearch from "../../components/LogoSearch/LogoSearch";
 
 const Chat = () => {
-  return <div className="Chat">Chat</div>;
+  return (
+    <div className="Chat">
+      {/* LeftSide */}
+      <div className="Left-side-chat">
+        <LogoSearch />
+        <div className="Chat-container">
+          <h2>Chats</h2>
+          <div className="Chat-list">conversations</div>
+        </div>
+      </div>
+
+      {/* RightSide */}
+      <div className="Right-side-chat">Right side</div>
+    </div>
+  );
 };
 
 export default Chat;
