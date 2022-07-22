87474a2344c71e86a8730dbba0ab26a81335dbd4
IMRANDIL <aliimranadil2@gmail.com>
2022-07-22 19:30:04 +0530

    yh

---
+++ socket/index.js
@@ -0,0 +1,28 @@
+const io = require("socket.io")(8800, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let activeUsers = [];
+
+io.on("connection", (socket) => {
+  //add new user...
+
+  socket.on("new-user-add", (newUserId) => {
+    //if user is not in activeUsers array
+    if (!activeUsers.includes(newUserId)) {
+      activeUsers.push({
+        userId: newUserId,
+        socketId: socket.id,
+      });
+    }
+    console.log("connected users: ", activeUsers);
+    io.emit("get-users", activeUsers);
+  });
+  socket.on("disconnect", () => {
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    console.log("User Disconnected", activeUsers);
+    io.emit("get-users", activeUsers);
+  });
+});
